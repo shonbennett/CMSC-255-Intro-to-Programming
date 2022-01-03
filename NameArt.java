b57dbb71d100380e55af97bf501e96505bd9c52e
shonbennett <97065099+shonbennett@users.noreply.github.com>
2022-01-03 17:23:14 -0500

    Create NameArt.java

---
+++ NameArt.java
@@ -0,0 +1,30 @@
+/* 
+*
+*NameArt
+*
+*This project outputs my name by printing the big letters with smaller versions of them. S's make up the S, H's make up the H and so on.
+*
+*Shon Bennett
+*September 2, 2021
+*CMSC 255 Section 
+*/
+public class NameArt {
+	public static void main(String[] args)	{
+		System.out.println("Shon Bennett");
+		System.out.println("Project 1- Name In Code");
+		System.out.println("This project outputs my name by printing the big letters with smaller versions of them. S's make up the S, H's make up the H and so on.");
+		System.out.println("September 2, 2021");
+		System.out.println("CMSC 255 Section");
+		System.out.println("");
+		System.out.println("SSSSSSSSSSSSS  HHHHHH      HHHHHH  OOOOOOOOOOOOOOOOOO  NNNNNNN     NNNNNN");
+		System.out.println("SSSSSSSSSSSSS  HHHHHH      HHHHHH  OOOOOOOOOOOOOOOOOO  NNNNNN N    NNNNNN");
+		System.out.println("SSSSSS         HHHHHH      HHHHHH  OOOOOO      OOOOOO  NNNNNN  N   NNNNNN");
+		System.out.println("SSSSSS         HHHHHH      HHHHHH  OOOOOO      OOOOOO  NNNNNN   N  NNNNNN");
+		System.out.println("SSSSSSSSSSSSS  HHHHHHHHHHHHHHHHHH  OOOOOO      OOOOOO  NNNNNN    N NNNNNN");
+		System.out.println("SSSSSSSSSSSSS  HHHHHHHHHHHHHHHHHH  OOOOOO      OOOOOO  NNNNNN     NNNNNNN");
+		System.out.println("      SSSSSSS  HHHHHH      HHHHHH  OOOOOO      OOOOOO  NNNNNN      NNNNNN");
+		System.out.println("      SSSSSSS  HHHHHH      HHHHHH  OOOOOO      OOOOOO  NNNNNN      NNNNNN");
+		System.out.println("SSSSSSSSSSSSS  HHHHHH      HHHHHH  OOOOOOOOOOOOOOOOOO  NNNNNN      NNNNNN");
+		System.out.println("SSSSSSSSSSSSS  HHHHHH      HHHHHH  OOOOOOOOOOOOOOOOOO  NNNNNN      NNNNNN");
+	}
+}
